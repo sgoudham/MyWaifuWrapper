aab72afde1dae29e11d2711b3f634eb0537d8b5a
Hammy <sgoudham@gmail.com>
2021-06-08 19:37:37 +0100

    Delete unused code

--- src/main/java/me/goudham/Response.java
+++ src/main/java/me/goudham/Response.java
@@ -17,7 +17,7 @@ import me.goudham.api.entity.waifu.Waifu;
  *
  */
 public class Response<T> {
-    private T entity;
+    private final T entity;
     private final Integer responseCode;
     private final String responseBody;
 
@@ -25,11 +25,6 @@ public class Response<T> {
         this.responseCode = responseCode;
         this.responseBody = responseBody;
         this.entity = entity;
-    }
-
-    Response(Integer responseCode, String responseBody) {
-        this.responseCode = responseCode;
-        this.responseBody = responseBody;
     }
 
     public T getEntity() {
